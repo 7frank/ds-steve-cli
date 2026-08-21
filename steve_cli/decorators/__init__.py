3916cb68c728b2d82c2d290ca3a51cf715f2b4af
Frank <frank.reimann@jambit.de>
2026-08-21 14:47:03 +0200

    add decorators

--- steve_cli/decorators/__init__.py
+++ steve_cli/decorators/__init__.py
@@ -1,3 +1,10 @@
+from collections.abc import Callable
+
+from steve_cli.lineage.storage import LineageStorage
+
 from .lineage_job import lineage_job
 
-__all__ = ["lineage_job"]
+GetTables = Callable[[str, str | None], LineageStorage]
+GetStorage = Callable[[str, str | None], LineageStorage]
+
+__all__ = ["GetStorage", "GetTables", "lineage_job"]
